82b71a30d89e42e7224ba7fc78d8a5372bf4ec33
ryo-8192 <ryouhei3213@gmail.com>
2026-09-10 15:02:19 +0900

    Intelijからテストコミット

--- backend/src/main/java/com/example/taskboard/repository/TaskRepository.java
+++ backend/src/main/java/com/example/taskboard/repository/TaskRepository.java
@@ -3,5 +3,6 @@ package com.example.taskboard.repository;
 import com.example.taskboard.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// テストコミット用コメント
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
