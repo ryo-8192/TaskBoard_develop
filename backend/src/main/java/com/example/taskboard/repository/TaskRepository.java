7c9d7f087015aaaebaf8abf654ecf1f2f7d3a01b
ryo-8192 <ryouhei3213@gmail.com>
2026-09-16 23:22:21 +0900

    テスト用コミット用コメント削除

--- backend/src/main/java/com/example/taskboard/repository/TaskRepository.java
+++ backend/src/main/java/com/example/taskboard/repository/TaskRepository.java
@@ -3,6 +3,5 @@ package com.example.taskboard.repository;
 import com.example.taskboard.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// テストコミット用コメント
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
